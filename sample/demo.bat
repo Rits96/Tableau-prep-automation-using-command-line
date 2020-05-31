75659d00f8f0e18a4bc19f6c8ab123b61aa03ef3
Rits96 <rutujauttarwar1996@gmail.com>
2020-05-31 11:17:35 +0530

    Add files via upload

---
+++ sample/demo.bat
@@ -0,0 +1,4 @@
+cd\
+cd "Program Files\Tableau\Tableau Prep Builder 2019.3\scripts\"
+tableau-prep-cli.bat  -t "C:\Users\111
+\Desktop\TableauPrep\demo.tfl"
---
+++ sample/sql2.bat
@@ -0,0 +1,3 @@
+cd\
+cd "Program Files\Tableau\Tableau Prep Builder 2019.3\scripts\"
+tableau-prep-cli.bat -c "C:\Users\111\Desktop\sample\cred2.json" -t "C:\Users\111\Desktop\sample\MySQL2.tfl"
